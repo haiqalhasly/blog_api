a1551f79db006ef468f508f27fef90bd27d952db
haiqalhasly <haiqalhasly@gmail.com>
2025-10-19 17:29:53 +0800

    Refactor post creation logic and enhance JSON response handling

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 import json
 app = Flask(__name__)
-
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -18,11 +17,6 @@ mysql = MySQL(app)
 def hello():
     return 'Hello'
 
-# Load the JSON data
-input_file= "posts.json"
-with open(input_file, encoding="utf-8") as json_file:
-    parsed_json = json.load(json_file)
-
 @app.route("/posts", methods=["GET"])
 def get_post():
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -30,6 +24,28 @@ def get_post():
     account = cursor.fetchone()
     return account
 
+@app.route('/posts', methods=['POST'])
+def create_post():
+    data = request.get_json()
+
+    title = data.get('title')
+    content = data.get('content')
+    category = data.get('category')
+    tags = data.get('tags')
+
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    sql = "INSERT INTO posts (title, content, category, tags) VALUES (%s, %s, %s, %s)"
+    val = (title, content, category, tags)
+    cursor.execute(sql, val)
+
+    mysql.connection.commit()
+
+    # 4. Done (return success)
+    return jsonify({
+        "message": "Post created successfully", 
+        "id": cursor.lastrowid # Tell the user the ID of the new post
+    }), 201 # 201 is the HTTP status code for 'Created'
 
 
 # Flask will always ask for the browser icon, this will ignore it  
